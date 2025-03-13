2278295b3c9a8e87289213a426f4bf9b160ba49a
LloydC <lloydchambrier@gmail.com>
2025-03-13 20:49:00 +0100

    lesson-11 code add

---
+++ module3/lesson-11/file-upload/client/src/App.jsx
@@ -0,0 +1,59 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage/HomePage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import SignupPage from "./pages/SignupPage/SignupPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import AddMovie from "./pages/AddMovie/AddMovie";
+
+import Navbar from "./components/Navbar/Navbar";
+import IsPrivate from "./components/IsPrivate/IsPrivate";
+import IsAnon from "./components/IsAnon/IsAnon";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route
+          path="/profile"
+          element={
+            <IsPrivate>
+              <ProfilePage />
+            </IsPrivate>
+          }
+        />
+
+        <Route
+          path="/signup"
+          element={
+            <IsAnon>
+              <SignupPage />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <IsAnon>
+              <LoginPage />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/add-movie"
+          element={
+           
+              <AddMovie />
+          
+          } />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
